93ed3b09a202d58e4d315035186e60dfac40df05
ersinsari13 <sariiersinn13@gmail.com>
2023-12-22 14:29:01 +0300

    update file

--- jenkins/build-prod-docker-images-for-ecr.sh
+++ jenkins/build-prod-docker-images-for-ecr.sh
@@ -1,3 +1,3 @@
 docker build --force-rm -t "${IMAGE_TAG_PETCLINIC}" .
 docker build --force-rm -t "${IMAGE_TAG_GRAFANA_SERVICE}" "${WORKSPACE}/pro-gra/grafana"
-docker build --force-rm -t "${IMAGE_TAG_PROMETHEUS_SERVICE}" "${WORKSPACE}/progra/prometheus"
+docker build --force-rm -t "${IMAGE_TAG_PROMETHEUS_SERVICE}" "${WORKSPACE}/pro-gra/prometheus"
