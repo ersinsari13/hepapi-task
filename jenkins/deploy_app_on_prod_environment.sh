c7f53108ff95fedd90a8a3bf78a3124a1e514810
ersinsari13 <sariiersinn13@gmail.com>
2023-12-24 14:21:21 +0300

    update dev branch

--- jenkins/deploy_app_on_prod_environment.sh
+++ jenkins/deploy_app_on_prod_environment.sh
@@ -2,7 +2,7 @@ echo 'Deploying App on Kubernetes'
 envsubst < k8s/petclinic_chart/values-template.yaml > k8s/petclinic_chart/values.yaml
 sed -i s/HELM_VERSION/${BUILD_NUMBER}/ k8s/petclinic_chart/Chart.yaml
 cd /var/lib/jenkins/chart-repo
-helm package /var/lib/jenkins/petclinic-argocd/k8s/petclinic_chart
+helm package /var/lib/jenkins/workspace/petclinic-argocd/k8s/petclinic_chart
 helm repo index .
 git add .
 git commit "petclinic_chart-${BUILD_NUMBER}.tgz added"
